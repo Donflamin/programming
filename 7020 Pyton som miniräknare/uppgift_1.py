832af4c4e03ee2037523aaac870ad6ccde999651
Sander <sander.naranjo.olsson05@skola.stenungsund.se>
2023-09-25 13:28:23 +0200

    uppgift 1 & 2

---
+++ 7020 Pyton som miniräknare/uppgift_1.py
@@ -0,0 +1,13 @@
+# uppgift 1A
+
+print(4+4)
+print(9-1)
+print(10-2)
+print(5000-4992)
+print(1+1+1+1+1+1+1+1)
+print(6+2)
+print(7+1)
+
+# uppgift 1B
+#
+# Division
---
+++ 7020 Pyton som miniräknare/uppgift_2.py
@@ -0,0 +1,9 @@
+# uppgift 2A
+
+print(6+10.0)
+print(2.0+14)
+print(5+11.0)
+print(1.0+15)
+print(9.0+7)
+print(2.0+2+2+10)
+print(20.0-4)
