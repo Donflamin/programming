221f001f3b70850af099f3b8240c0299a9a957e1
Sander <sander.naranjo.olsson05@skola.stenungsund.se>
2023-10-12 13:16:20 +0200

    e

---
+++ 7095 for-slingor med range/uppgift_1.py
@@ -0,0 +1,24 @@
+#uppgift 1A
+
+for i in range(5):
+    print(i, "uppgiftA")
+
+#uppgift 1B
+
+for i in range(3, 8):
+    print(i, "uppgiftB")
+
+#uppgift 1C
+
+for i in range(4, 20, 3):
+    print(i, "uppgiftC")
+
+#uppgift 1D
+
+for i in range(20, 41, 5):
+    print(i, "uppgiftD")
+
+#uppgift 1E
+
+for i in range(10, -1, -1):
+    print(i, "uppgiftE")
--- loggbok.md
+++ loggbok.md
@@ -2,3 +2,6 @@
 
 började loggboken och githubben den 18 september.
 
+---
+
+12 october. gått bra med uppgifter, erkänner dock att det är rätt många. har valt att göra spel i Godot som projekt. ska använda mig av GDScript.
